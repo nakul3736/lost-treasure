254f3709583e6c24f883a369297aef8292d522a5
unknown <nakul13122001@gmail.com>
2022-05-21 15:37:07 +0530

    added home page and main page

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use("/images", express.static(__dirname + "/public/images"));
+app.use("/app", express.static(__dirname + "/public/app"));
+app.use("/css", express.static(__dirname + "/public/css"));
+
+app.get("/buried_treasure", (req, res) => {
+  res.sendFile("./views/buried_treasure.html", {
+    root: __dirname + "/public",
+  });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/views/index.html");
+});
+
+app.listen(PORT, function () {
+  console.log(`server running on port ${PORT}`);
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname + "/public" });
+});
